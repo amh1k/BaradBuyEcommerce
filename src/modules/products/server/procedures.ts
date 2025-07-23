9c2d62346ca7d8b20f7165d9f99299487f166bc0
amh1k <abdulmoizx97@gmail.com>
2025-07-23 11:09:23 +0500

    Added products ui

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -7,10 +7,13 @@ import { Category, Media } from "@/payload-types";
 import configPromise from "@payload-config";
 import { InputOTP } from "@/app/(app)/components/ui/input-otp";
 import { sortValues } from "../hooks/search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
@@ -92,6 +95,8 @@ export const productsRouter = createTRPCRouter({
         depth: 1,
         where,
         sort,
+        page: input.cursor,
+        limit: input.limit,
       });
 
       return {
