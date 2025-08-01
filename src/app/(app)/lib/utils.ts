85197888e4d603004bd27a2af19d97cdb5697b33
amh1k <abdulmoizx97@gmail.com>
2025-08-01 20:04:23 +0500

    Deployment fixes

--- src/app/(app)/lib/utils.ts
+++ src/app/(app)/lib/utils.ts
@@ -8,9 +8,13 @@ export function generateTenantURL(tenantSlug: string) {
   // if (process.env.NODE_ENV === "development") {
   //   return `/tenants/${tenantSlug}`;
   // }
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled =
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING === "true";
+
   const protocol = "https";
   const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
-  if (process.env.NODE_ENV === "development") {
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
   return `${protocol}://${tenantSlug}.${domain}`;
